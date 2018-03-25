9f86a558fbd7c8b1667256598c61172b97b84305
James Ko <jamesqko@gmail.com>
2018-03-25 05:01:15 -0400

    App logic works

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Button,
   Image,
   StyleSheet,
   Text,
@@ -7,26 +8,30 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
+import Duration from './Duration';
+
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
+
     this.state = {
       intervalId: -1,
       isTicking: false,
-      originalDuration: new Duration(),
       timerDuration: new Duration()
     };
+
+    this.handleStart = this.handleStart.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleClear = this.handleClear.bind(this);
   }
 
   handleStart() {
     var intervalId = setInterval(() => {
       this.setState(previousState => {
-        assert(previousState.intervalId !== -1);
         return {
           intervalId: previousState.intervalId,
           isTicking: previousState.isTicking,
-          originalDuration: previousState.originalDuration,
           timerDuration: previousState.timerDuration.tick()
         };
       });
@@ -36,9 +41,8 @@ export default class App extends Component<Props> {
       return {
         intervalId: intervalId,
         isTicking: true,
-        originalDuration: previousState.originalDuration,
-        timerDuration: previousState.originalDuration
-      }
+        timerDuration: previousState.timerDuration
+      };
     });
   }
 
@@ -49,9 +53,8 @@ export default class App extends Component<Props> {
       return {
         intervalId: -1,
         isTicking: false,
-        originalDuration: new Duration(),
-
-      }
+        timerDuration: previousState.timerDuration
+      };
     });
   }
 
@@ -60,7 +63,9 @@ export default class App extends Component<Props> {
 
     this.setState(previousState => {
       return {
-        intervalId
+        intervalId: -1,
+        isTicking: false,
+        timerDuration: new Duration()
       };
     });
   }
@@ -70,9 +75,10 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <LinearGradient colors={['#ff00a0', '#ffe5b4']} style={styles.linearGradient}>
           <Image source={require('./img/panel.png')} style={styles.panel} />
-          <Button onClick={this.handleStart} style={styles.commandButton} />
-          <Button onClick={this.handleStop} style={styles.commandButton} />
-          <Button onClick={this.handleClear} style={styles.commandButton} />
+          <Button title="Start" onPress={this.handleStart} style={styles.commandButton} />
+          <Button title="Stop" onPress={this.handleStop} style={styles.commandButton} />
+          <Button title="Clear" onPress={this.handleClear} style={styles.commandButton} />
+          <Text style={styles.timerText}>Hello world. {this.state.timerDuration.toString()}</Text>
         </LinearGradient>
       </View>
     );
@@ -95,5 +101,7 @@ const styles = StyleSheet.create({
   panel: {
     height: 375,
     aspectRatio: 0.9,
-  }
+  },
+  timerText: {
+  },
 });
--- Duration.js
+++ Duration.js
@@ -1,38 +1,49 @@
-export default class Duration {
-    constructor() {
-        this(0, 0, 0);
-    }
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+        padString = String((typeof padString !== 'undefined' ? padString : ' '));
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
 
-    constructor(hours, minutes, seconds) {
+export default class Duration {
+    constructor(hours = 0, minutes = 0, seconds = 0) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
-
-    get _isZero() {
-        return this.hours == 0 &&
-               this.minutes == 0 &&
-               this.seconds == 0;
+    
+    toString() {
+        return this.hours.toString().padStart(2, '0') + ':' +
+               this.minutes.toString().padStart(2, '0') + ':' +
+               this.seconds.toString().padStart(2, '0');
     }
 
     tick() {
-        if (this._isZero) {
-            throw new Error("Can't tick any more.");
-        }
-        
         var newHours = this.hours,
             newMinutes = this.minutes,
             newSeconds = this.seconds;
 
-        if (this.seconds != 0) {
-            newSeconds--;
-        } else if (this.minutes != 0) {
-            newSeconds = 59;
-            newMinutes--;
+        if (this.seconds != 59) {
+            newSeconds++;
+        } else if (this.minutes != 59) {
+            newSeconds = 0;
+            newMinutes++;
         } else {
-            newSeconds = 59;
-            newMinutes = 59;
-            newHours--;
+            newSeconds = 0;
+            newMinutes = 0;
+            newHours++;
         }
 
         return new Duration(
